5ee7f392fdbc158a4dd6083269dc60e9f5a100bd
quochieu97 <ngohieu209@gmail.com>
2024-01-12 11:53:16 +0700

    KTCK
    ktra

---
+++ Project_KTCK/assets/js/main1.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:24,
+        nav:true,
+        dots: false,
+        // autoplay:true,
+        // autoplayTimeout: 3000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+    
+  });
+  
